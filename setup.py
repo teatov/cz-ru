fbd753ca56b167215f7ce6361d4843239f49d9d6
teatov <hlebpoimenigleb@gmail.com>
2024-11-01 04:11:56 +1000

    fix(setup.py): в `long_description` добавлено содержимое `README.md`

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+import os
 from setuptools import setup
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+filepath = os.path.join(dir_path, "README.md")
+with open(filepath) as f:
+    readme = f.read()
 
 setup(
     name="cz_ru",
     version="0.1.1",
     py_modules=["cz_ru"],
     license="MIT",
-    long_description="Конфиг для Commitizen на русском языке, основанный на Conventional Commits",
+    long_description=readme,
     url="https://github.com/teatov/cz-ru",
     install_requires=["commitizen"],
     entry_points={

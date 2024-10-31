c2934e41fc906159d3025fe0bdd2f87b73d4ad3b
teatov <hlebpoimenigleb@gmail.com>
2024-11-01 04:11:16 +1000

    Revert "fix(README.md): в `long_description` добавлено содержимое `README.md`"
    This reverts commit ac32db8f154c6b11fed46f6aa6a63f1134d13451.

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
-import os
 from setuptools import setup
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-filepath = os.path.join(dir_path, "cz_ru_info.txt")
-with open(filepath) as f:
-    readme = f.read()
 
 setup(
     name="cz_ru",
     version="0.1.1",
     py_modules=["cz_ru"],
     license="MIT",
-    long_description=readme,
+    long_description="Конфиг для Commitizen на русском языке, основанный на Conventional Commits",
     url="https://github.com/teatov/cz-ru",
     install_requires=["commitizen"],
     entry_points={

e6687e8088552c0c6bc962c9de1192eeda33d07f
teatov <hlebpoimenigleb@gmail.com>
2024-11-01 03:43:22 +1000

    build(setup.py): дополнено `long_description`

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.1.0",
     py_modules=["cz_ru"],
     license="MIT",
-    long_description="Конфиг для Commitizen на русском языке",
+    long_description="Конфиг для Commitizen на русском языке, основанный на Conventional Commits",
     url="https://github.com/teatov/cz-ru",
     install_requires=["commitizen"],
     entry_points={

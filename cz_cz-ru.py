4d82b91ed571d2412a69e3e05e4a34039d1e1f4f
teatov <hlebpoimenigleb@gmail.com>
2024-11-01 00:47:22 +1000

    Скопирован конфиг `commitizen/cz/conventional_commits/conventional_commits.py`

--- cz_cz-ru.py
+++
@@ -1,9 +0,0 @@
-from commitizen.cz.base import BaseCommitizen
-
-
-class CzRu(BaseCommitizen):
-    def questions(self) -> list:
-        raise NotImplementedError("Not Implemented yet")
-
-    def message(self, answers: dict) -> str:
-        raise NotImplementedError("Not Implemented yet")
--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from setuptools import setup
 
 
 setup(
-    name="cz-ru",
+    name="cz_ru",
     version="0.1.0",
-    py_modules=["cz_cz-ru"],
+    py_modules=["cz_ru"],
     license="MIT",
     long_description="Конфиг для Commitizen на русском языке",
+    url="https://github.com/teatov/cz-ru",
     install_requires=["commitizen"],
+    entry_points={
+        "commitizen.plugin": [
+            "cz_ru = cz_ru:CzRu"
+        ]
+    },
 )
